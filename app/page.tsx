dbb8a7cc35daac7fe8f08f550a03d03750d87318
hari1275 <khari1275@gmail.com>
2024-09-29 20:29:31 +0530

    moved page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
-import HomePage from './(home)/page';
-import { metadata } from './metadata';
+import type { Metadata } from 'next';
+import ConvertSection from './(home)/ConvertSection';
+import Hero from './(home)/Hero';
+import InnovationsSection from './(home)/InnovationsSection';
+import TrustedBy from './(home)/TrustedBy';
+import UnlockSection from './(home)/UnlockSection';
 
-export { metadata };
+export const metadata: Metadata = {
+  title: 'Home',
+  description:
+    'Welcome to Articul8 - The GenAI platform that brings order to chaos.',
+};
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <TrustedBy />
+      <UnlockSection />
+      <InnovationsSection />
+      <ConvertSection />
+      {/* <Features />
+      <BlogPreview />
+      <Contact /> */}
+    </div>
+  );
+};
 
 export default HomePage;
