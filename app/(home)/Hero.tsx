90c21a9fa75498dacac3f1de1ff0970f097f9b03
hari1275 <khari1275@gmail.com>
2024-10-20 20:47:23 +0530

    animation glitch

--- app/(home)/Hero.tsx
+++ app/(home)/Hero.tsx
@@ -17,15 +17,15 @@ export default function Hero() {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
         setTimeout(() => {
           setIsExpanded(true);
-        }, 100);
-      }, 400);
-    }, 3000);
+        }, 600);
+      }, 200);
+    }, 2000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <section className='relative sm:min-h-[85vh] flex items-center justify-center bg-[#112FFF] text-white overflow-hidden'>
+    <section className='relative sm:min-h-[80vh] flex items-center justify-center bg-[#112FFF] text-white overflow-hidden'>
       <div className='container mx-auto px-6 flex flex-col lg:flex-row items-center lg:items-start h-full'>
         <div className='text-left w-full  z-10 mb-8 lg:mb-0'>
           <h1 className='font-space-grotesk text-[32px]  font-[700]  sm:py-8 pt-12 sm:text-[50px] md:text-[54px] lg:text-[64px] sm:leading-[76.8px] md:leading-[76.8px] lg:leading-[76.8px] text-center lg:text-left'>
@@ -60,7 +60,7 @@ export default function Hero() {
                         y: { duration: 0.3, ease: [0.33, 1, 0.68, 1] },
                         opacity: { duration: 0.2, ease: 'easeInOut' },
                       }}
-                      className='inline-block whitespace-nowrap'
+                      className='inline-block whitespace-nowrap px-1'
                       style={{ display: 'block', position: 'relative' }}
                     >
                       {words[currentIndex]}
@@ -72,7 +72,7 @@ export default function Hero() {
                   alt='Close bracket'
                   width={32}
                   height={32}
-                  className='mr-1 w-[20px] sm:w-[30px] md:w-[32px] h-auto'
+                  className='ml-1 w-[20px] sm:w-[30px] md:w-[32px] h-auto'
                   priority
                 />
               </motion.div>
@@ -84,7 +84,7 @@ export default function Hero() {
             Bring order to chaos.
           </p>
         </div>
-        <div className='w-full lg:w-[58%]  lg:absolute lg:right-0 lg:top-0 lg:bottom-0'>
+        <div className='w-full lg:w-[52%]  lg:absolute lg:right-0 lg:top-0 lg:bottom-0'>
           <div className='relative w-full h-[378px] sm:h-[640px]  lg:h-full'>
             <Image
               src='/images/hero-cubes.svg'
@@ -93,7 +93,7 @@ export default function Hero() {
               objectFit='contain'
               objectPosition='center'
               priority
-              className='scale-100 lg:scale-100 lg:translate-x-[2%] lg:translate-y-[8%]'
+              className='scale-100 lg:scale-100 lg:translate-x-[5%] lg:translate-y-[12%]'
             />
           </div>
         </div>
