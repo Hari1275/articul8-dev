1dc93e37808d503ab3b340e5f53fa2e23eb57caf
Hariprasad K <53053015+Hari1275@users.noreply.github.com>
2024-10-28 23:14:02 +0530

    Update Hero.tsx

--- app/(about)/about/Hero.tsx
+++ app/(about)/about/Hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className='relative h-[50vh] sm:h-[80vh]'>
+    <section className='relative h-[60vh] sm:h-[100vh]'>
       <video
         src='/images/icons/about/about-hero.mp4'  
         autoPlay
